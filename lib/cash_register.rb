19d499c0befdbbb25484e22a832d552ad0cc8a91
Jane Justiz <janejustiz@gmail.com>
2018-07-12 03:23:57 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   def add_item(title, price, quantity = 0)
     @@items = []
     @total += price
-    @@items << title
+    self.@@items << title
   end
 
   def apply_discount
