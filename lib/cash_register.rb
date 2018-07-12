ef7beeee376c892123373606ba11580fdc1c15b8
Jane Justiz <janejustiz@gmail.com>
2018-07-12 03:40:19 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,8 +12,8 @@ class CashRegister
     @total
   end
 
-  def add_item(title, price, quantity = 0)
-    @total += price
+  def add_item(title, price, quantity = 1)
+    @total += price.times(quantity)
     @@items << title
   end
 
